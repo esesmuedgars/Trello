cd86a3c13789ddfdc4c6dc0c65bbb4844178efee
Edgars Vanags <e.vanags@accenture.com>
2019-04-14 01:09:57 +0300

    Added assignment helpers

--- trello/ViewController.swift
+++ trello/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    /// Defines how the token is returned to client.
+    /// Should not be edited.
+    let callbackMethod = "fragment"
 
+    /// Redirect URL where the user will be redirected after authorization.
+    /// Should not be edited.
+    let returnUrl = "mytrelloapp:"
+
+    /// Permission scope.
+    ///
+    /// - `read`
+    /// reading of boards, organizations, etc. on behalf of the user.
+    ///
+    /// - `write`
+    /// writing of boards, organizations, etc. on behalf of the user.
+    ///
+    /// - `account`
+    /// writing of member info, and marking notifications read.
+    let scope = "<#String#>"
+
+    /// Indicator of when the token should expire.
+    let expiration = "<#String#>"
+
+    /// Name of the application.
+    /// Displayed during the authorization process.
+    let name = "<#String#>"
+
+    /// API key.
+    /// Used to generate the user's token.
+    let key = "<#String#>"
+
+    /// Response type of token.
+    /// Should not be edited.
+    let responseType = "token"
 
 }
 
