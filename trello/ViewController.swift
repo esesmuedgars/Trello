134f7459479b66cc0935c8c1dc342dba49e2593d
Edgars Vanags <e.vanags@accenture.com>
2019-04-13 22:20:18 +0300

    Created project

---
+++ trello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trello
+//
+//  Created by e.vanags on 13/04/2019.
+//  Copyright © 2019 esesmuedgars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
